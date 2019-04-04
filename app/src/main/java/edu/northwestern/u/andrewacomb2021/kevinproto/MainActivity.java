2799bca1d944d107b771b28ebcc61b463f62df25
changdaniel116 <changdaniel116@yahoo.com>
2019-04-04 01:55:06 -0500

    Conditional Presentation with Tech Demo

--- app/src/main/java/edu/northwestern/u/andrewacomb2021/kevinproto/MainActivity.java
+++ app/src/main/java/edu/northwestern/u/andrewacomb2021/kevinproto/MainActivity.java
@@ -113,6 +113,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, Tutorial.class);
                 startActivity(intent);
+
             }
         });
 
